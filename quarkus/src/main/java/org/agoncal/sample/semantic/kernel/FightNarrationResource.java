78d89c94c7f2c3e0bd271ec8d1146db97c3cd44b
Antonio Goncalves <antonio.goncalves@gmail.com>
2023-08-16 16:50:33 +0200

    Added SK

--- quarkus/src/main/java/org/agoncal/sample/semantic/kernel/FightNarrationResource.java
+++ quarkus/src/main/java/org/agoncal/sample/semantic/kernel/FightNarrationResource.java
@@ -1,16 +1,58 @@
 package org.agoncal.sample.semantic.kernel;
 
+import com.azure.ai.openai.OpenAIAsyncClient;
+import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.SKBuilders;
+import com.microsoft.semantickernel.connectors.ai.openai.util.OpenAIClientProvider;
+import com.microsoft.semantickernel.exceptions.ConfigurationException;
+import com.microsoft.semantickernel.orchestration.SKContext;
+import com.microsoft.semantickernel.skilldefinition.ReadOnlyFunctionCollection;
+import com.microsoft.semantickernel.textcompletion.CompletionSKFunction;
+import com.microsoft.semantickernel.textcompletion.TextCompletion;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
+import reactor.core.publisher.Mono;
 
 @Path("/narration")
 public class FightNarrationResource {
 
+  @Inject
+  Logger LOGGER;
+
   @GET
   @Produces(MediaType.TEXT_PLAIN)
-  public String hello() {
-    return "Hello RESTEasy";
+  public String narrateAFightInOnePrompt() throws ConfigurationException {
+
+    // Creates an Azure OpenAI client
+    OpenAIAsyncClient client = OpenAIClientProvider.getClient();
+
+    // Creates an instance of the TextCompletion service
+    TextCompletion textCompletion = SKBuilders.chatCompletion().withOpenAIClient(client).setModelId("deploy-semantic-kernel").build();
+
+    // Instantiates the Kernel
+    Kernel kernel = SKBuilders.kernel().withDefaultAIService(textCompletion).build();
+
+    // Registers skills
+    ReadOnlyFunctionCollection skill = kernel.importSkillFromDirectory("SuperHeroesSkill", "src/main/resources", "SuperHeroesSkill");
+    CompletionSKFunction fightFunction = skill.getFunction("FightInOnePrompt", CompletionSKFunction.class);
+
+    // Ask for a Joke
+    SKContext fightContext = SKBuilders.context().build();
+    fightContext.setVariable("villain_name", "Darth Vader");
+    fightContext.setVariable("villain_powers", "Accelerated Healing, Agility, Astral Projection, Cloaking, Danger Sense, Durability, Electrokinesis, Energy Blasts, Enhanced Hearing, Enhanced Senses, Force Fields, Hypnokinesis, Illusions, Intelligence, Jump, Light Control, Marksmanship, Precognition, Psionic Powers, Reflexes, Stealth, Super Speed, Telekinesis, Telepathy, The Force, Weapons Master");
+    fightContext.setVariable("villain_level", "13");
+    fightContext.setVariable("hero_name", "Chewbacca");
+    fightContext.setVariable("hero_powers", "Agility, Longevity, Marksmanship, Natural Weapons, Stealth, Super Strength, Weapons Master");
+    fightContext.setVariable("hero_level", "5");
+    Mono<SKContext> result = fightFunction.invokeAsync(fightContext);
+
+    String narration = result.block().getResult();
+    LOGGER.info("The narration for the fight is: " + narration);
+
+    return narration;
   }
 }
