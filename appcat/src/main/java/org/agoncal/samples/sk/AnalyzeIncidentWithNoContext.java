de97443e5294da37f05fb3464e478c134fd983dc
Antonio Goncalves <antonio.goncalves@gmail.com>
2023-08-18 15:38:55 +0200

    GPT 3.5 16k

--- appcat/src/main/java/org/agoncal/samples/sk/AnalyzeIncidentWithNoContext.java
+++ appcat/src/main/java/org/agoncal/samples/sk/AnalyzeIncidentWithNoContext.java
@@ -22,7 +22,7 @@ public class AnalyzeIncidentWithNoContext {
     OpenAIAsyncClient client = OpenAIClientProvider.getClient();
 
     // Creates an instance of the TextCompletion service
-    TextCompletion textCompletion = SKBuilders.chatCompletion().withOpenAIClient(client).setModelId("deploy-semantic-kernel").build();
+    TextCompletion textCompletion = SKBuilders.chatCompletion().withOpenAIClient(client).setModelId("deploy-semantic-kernel-16k").build();
 
     // Instantiates the Kernel
     Kernel kernel = SKBuilders.kernel().withDefaultAIService(textCompletion).build();
