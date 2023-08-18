0546ae2aaf6a44fb3ef99d72336a41c3970e26ad
Antonio Goncalves <antonio.goncalves@gmail.com>
2023-08-18 14:51:04 +0200

    AppCAT samples

---
+++ appcat/src/main/java/org/agoncal/samples/sk/AnalyzeIncidentWithContext.java
@@ -0,0 +1,40 @@
+package org.agoncal.samples.sk;
+
+import com.azure.ai.openai.OpenAIAsyncClient;
+import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.SKBuilders;
+import com.microsoft.semantickernel.connectors.ai.openai.util.OpenAIClientProvider;
+import com.microsoft.semantickernel.exceptions.ConfigurationException;
+import com.microsoft.semantickernel.orchestration.SKContext;
+import com.microsoft.semantickernel.skilldefinition.ReadOnlyFunctionCollection;
+import com.microsoft.semantickernel.textcompletion.CompletionSKFunction;
+import com.microsoft.semantickernel.textcompletion.TextCompletion;
+import org.slf4j.Logger;
+import reactor.core.publisher.Mono;
+
+public class AnalyzeIncidentWithContext {
+
+  private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(AnalyzeIncidentWithContext.class);
+
+  public static void main(String[] args) throws ConfigurationException {
+
+    // Creates an Azure OpenAI client
+    OpenAIAsyncClient client = OpenAIClientProvider.getClient();
+
+    // Creates an instance of the TextCompletion service
+    TextCompletion textCompletion = SKBuilders.chatCompletion().withOpenAIClient(client).setModelId("deploy-semantic-kernel").build();
+
+    // Instantiates the Kernel
+    Kernel kernel = SKBuilders.kernel().withDefaultAIService(textCompletion).build();
+
+    // Registers skills
+    ReadOnlyFunctionCollection skill = kernel.importSkillFromDirectory("AppCatSkill", "appcat/src/main/resources", "AppCatSkill");
+    CompletionSKFunction function = skill.getFunction("AnalyzeWithContext", CompletionSKFunction.class);
+
+    // Asks for an analysis
+    Mono<SKContext> result = function.invokeAsync();
+
+    LOGGER.info("The analysis is: \n\n{}", result.block().getResult());
+  }
+
+}
