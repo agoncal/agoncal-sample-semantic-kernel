2fc675324882aa9a9f4eaeb5cf6c7288d961ad7f
Antonio Goncalves <antonio.goncalves@gmail.com>
2023-11-10 16:17:47 +0100

    Quinoa

---
+++ chat/src/test/java/org/agoncal/samples/sk/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.agoncal.samples.sk;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
